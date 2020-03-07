a96bafbd8c153f2e1c0abb80b99ee3e67563dc22
Thomas Boerger <thomas@webhippie.de>
2020-03-07 17:06:07 +0100

    Prepare drone pipeline for lint, doctor and testing

--- doctor/HEADER.md
+++ doctor/HEADER.md
@@ -1,3 +1,3 @@
 # custodian
 
-[![Build Status](https://cloud.drone.io/api/badges/rolehippie/custodian/status.svg)](https://cloud.drone.io/rolehippie/mdadm)
+[![Build Status](https://cloud.drone.io/api/badges/rolehippie/custodian/status.svg)](https://cloud.drone.io/rolehippie/custodian)
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,13 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+
+def test_running_and_enabled(host):
+    svc = host.service("custodian.timer")
+    assert svc.is_running
+    assert svc.is_enabled
